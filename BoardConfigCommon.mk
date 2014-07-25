1b982908b96e88b56c4bdd8bbb31bdade7571357
Ethan Chen <intervigil@gmail.com>
2014-07-24 23:04:42 -0700

    msm8974-common: Clean up Samsung device inheritance
    * Remove duplicate keylayouts
    * Move media_codecs to platform tree
    * Unconditionally build consumerir
    * Move postrecoveryboot to qcom-common
    * Move common msm8974 HAL/packages to platform tree
    * Move wifi config to device trees
    * Use nested cm.dependencies
    * Generate firmware symlinks at compile time
    * Move egl.cfg to msm8974-common
    * Move QCOM_BSP to platform repos
    * Move telephony permissions to device
    * Move reboot to download option to qcom-common
    * Move common overlay options to qcom-common
    
    Change-Id: I493dcf24269e852e7819c045dc3afc5c47da176a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,118 +12,90 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BOARD_VENDOR := samsung
+# inherit from qcom-common
+-include device/samsung/qcom-common/BoardConfigCommon.mk
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8974
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 
-# inherit from qcom-common
--include device/samsung/qcom-common/BoardConfigCommon.mk
-
 # Architecture
-TARGET_CPU_SMP := true
-
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_CPU_VARIANT := krait
 
-# Wifi related defines
-WIFI_BAND := 802_11_ABG
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HAVE_SAMSUNG_WIFI := true
-
-ifeq ($(TARGET_MSM8974_COMMON_WLAN_VARIANT),prima)
-  BOARD_WLAN_DEVICE := qcwcn
-  BOARD_HAS_QCOM_WLAN := true
-  TARGET_USES_WCNSS_CTRL := true
-  WIFI_DRIVER_FW_PATH_STA   := "sta"
-  WIFI_DRIVER_FW_PATH_AP    := "ap"
-else
-  BOARD_WLAN_DEVICE := bcmdhd
-  WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-  WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-  WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
-  WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
-  WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
-endif
-
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
-ifeq ($(TARGET_MSM8974_COMMON_WLAN_VARIANT),prima)
-  BOARD_HAVE_BLUETOOTH_QCOM := true
-else
-  BOARD_HAVE_BLUETOOTH_BCM := true
-endif
-
-# NFC
-BOARD_HAVE_NFC := true
-
-# Vold
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_MAX_PARTITIONS := 28
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # Camera
 TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
 
-# Workaround to avoid issues with legacy liblights on QCOM platforms
-TARGET_PROVIDES_LIBLIGHT := true
+# Charger
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGING_CMDLINE_NAME := "androidboot.bootchg"
+BOARD_CHARGING_CMDLINE_VALUE := "true"
 
-# Charging Mode Override.
-BOARD_CHARGING_MODE_BOOTING_LPM := 
-
-# Audio
-BOARD_HAVE_SAMSUNG_AUDIO := true
-BOARD_USES_ALSA_AUDIO := true
-BOARD_USES_FLUENCE_INCALL := true
-BOARD_USES_FLUENCE_FOR_VOIP := true
-BOARD_USES_SEPERATED_AUDIO_INPUT := true
-TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-
-# QCOM support
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_QCOM_MEDIA_VARIANT := caf-new
+# Display
+BOARD_EGL_CFG := device/samsung/msm8974-common/configs/egl.cfg
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 TARGET_QCOM_DISPLAY_VARIANT := caf-new
-BOARD_USES_LEGACY_ALSA_AUDIO := 
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_USES_QCOM_BSP := true
+OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Media
+TARGET_QCOM_MEDIA_VARIANT := caf-new
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
-# Use retire fence from MDP driver
-TARGET_DISPLAY_USE_RETIRE_FENCE := true
+# Qualcomm support
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+TARGET_USES_QCOM_BSP := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
-        device/samsung/msm8974-common/sepolicy
+    device/samsung/msm8974-common/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-        file_contexts \
-        app.te \
-        bluetooth.te \
-        device.te \
-        domain.te \
-        drmserver.te \
-        file.te \
-        hci_init.te \
-        healthd.te \
-        init.te \
-        init_shell.te \
-        keystore.te \
-        kickstart.te \
-        mediaserver.te \
-        nfc.te \
-        rild.te \
-        surfaceflinger.te \
-        system.te \
-        ueventd.te \
-        wpa.te \
-        wpa_socket.te
+    file_contexts \
+    app.te \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    hci_init.te \
+    healthd.te \
+    init.te \
+    init_shell.te \
+    keystore.te \
+    kickstart.te \
+    mediaserver.te \
+    nfc.te \
+    rild.te \
+    surfaceflinger.te \
+    system.te \
+    ueventd.te \
+    wpa.te \
+    wpa_socket.te
+
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 28
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
