c3dca3c57cc1c0a78fe351d29ce4f097c7b6aacd
solk2 <solk@solk.org.ua>
2014-09-10 23:13:15 +0300

    msm8974-common: Allow device maintainers to implement their own IR HAL.
    Change-Id: Iff97686d83688f6748e0d1bb0fb8e166959af844

--- consumerir/Android.mk
+++ consumerir/Android.mk
@@ -15,6 +15,8 @@
 # HAL module implementation stored in
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 
+ifneq ($(TARGET_PROVIDES_CONSUMERIR_HAL),true)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -26,3 +28,5 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
