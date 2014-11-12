36437a0b1e28edca6bf932c5f07b7d37fedc274f
Christopher R. Palmer <crpalmer@gmail.com>
2014-11-12 11:11:54 -0500

    msm8974: camera: Add include needed for cm-12.0
    Change-Id: I7e07c71107e74cd5fefa8b9bca7b9edee458b6fb

--- camera/Android.mk
+++ camera/Android.mk
@@ -6,6 +6,9 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
