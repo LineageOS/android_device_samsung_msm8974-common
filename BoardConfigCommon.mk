0232f8c3ca3ff12bb1e34519760c798370b7d96e
elelinux <ele.linux@gmail.com>
2014-11-15 17:49:14 +0100

    remove classpath
    Change-Id: I517ccaddd76450e3d68f1979ddf95452ed260da1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,9 +39,6 @@ BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGING_CMDLINE_NAME := "androidboot.bootchg"
 BOARD_CHARGING_CMDLINE_VALUE := "true"
 
-# Classpath
-PRODUCT_BOOT_JARS := $(subst $(space),:,$(PRODUCT_BOOT_JARS))
-
 # Display
 BOARD_EGL_CFG := device/samsung/msm8974-common/configs/egl.cfg
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
