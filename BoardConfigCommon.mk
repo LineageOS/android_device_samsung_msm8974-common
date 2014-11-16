26d845133076228ee043e010dc79030456dc9292
Matt Filetto <matt.filetto@gmail.com>
2014-11-16 03:49:58 -0800

    msm8974-common: include an expanded selection of fonts
    Change-Id: Ia34fd24bdd6d572c178e006f7a5a31d014159143

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,6 +89,9 @@ BOARD_USES_QC_TIME_SERVICES := true
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 28
