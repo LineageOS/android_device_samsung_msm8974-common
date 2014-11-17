4375ed3da16ab134910889f87cbaeda79b9a400c
Christopher R. Palmer <crpalmer@gmail.com>
2014-11-17 06:12:50 -0500

    msm8974-common: Update media_codecs for L
    Change-Id: I43e9f833033751fe65f01bd3bcc9f320cdf538be

--- msm8974.mk
+++ msm8974.mk
@@ -110,6 +110,9 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml  \
     device/samsung/msm8974-common/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 PRODUCT_PACKAGES += \
