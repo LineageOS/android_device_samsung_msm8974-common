e5de9d4949e108dfe910b8ce893a0a98c97b0eb4
Matt Filetto <matt.filetto@gmail.com>
2014-12-30 17:37:30 -0800

    Revert "Revert "msm8974-common: Use AwesomePlayer by default""
    This reverts commit 8c73d23c89b72f5790b17e86481f170ad2e0060b.
    
    Change-Id: I53c7c6a1a2c59e9fd64f238ab2e7b45c2e593534

--- msm8974.mk
+++ msm8974.mk
@@ -39,7 +39,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=false \
-    av.offload.enable=true
+    av.offload.enable=true \
+    media.stagefright.use-awesome=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
