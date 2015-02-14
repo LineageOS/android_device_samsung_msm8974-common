8987acf776cecccfe8d2ae59dc5e94e0086ec78c
Christopher R. Palmer <crpalmer@gmail.com>
2015-02-14 05:00:30 -0500

    msm8974-common: Add property to indicate we support usb-otg
    Change-Id: If032f13f87ac756e110bd77f6c84e71b5e9a3153

--- msm8974.mk
+++ msm8974.mk
@@ -65,6 +65,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.isUsbOtgEnabled=true
+
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
