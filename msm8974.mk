ac629d0682c8cc20d26e30db5ec51fa4321850d8
Christopher R. Palmer <crpalmer@gmail.com>
2015-02-14 05:00:30 -0500

    msm8974-common: Add property to indicate we support usb-otg
    Change-Id: If032f13f87ac756e110bd77f6c84e71b5e9a3153

--- msm8974.mk
+++ msm8974.mk
@@ -40,7 +40,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=false \
     mm.enable.qcom_parser=3314291 \
-    av.offload.enable=true
+    av.offload.enable=true \
+    persist.sys.isUsbOtgEnabled=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
