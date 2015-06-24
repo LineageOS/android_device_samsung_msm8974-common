3fdf84171637fbd85a2c652b5a24682c65c4b7a3
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-06-24 11:58:31 -0400

    msm8974: Remove copying of ffmpeg media codecs
    * Handled by default now
    
    Change-Id: I410cd49d8c8ab098a4abbbf1f054956be3c7573c
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -108,7 +108,6 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml  \
