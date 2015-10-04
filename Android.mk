bdd096ed063a0536138877d7d99b8c8c5fcb4092
Christer <christer.bjor@gmail.com>
2015-10-04 04:54:02 +0200

    msm8974-common: fix keymaster symlink names
    Change-Id: Id33afe8b135c766c96bdd6406021e019639d683a

--- Android.mk
+++ Android.mk
@@ -54,14 +54,14 @@ $(DTCPIP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(DTCPIP_SYMLINKS)
 
 KM_IMAGES := \
-    keymaster.b00 keymaster.b01 keymaster.b02 keymaster.b03 keymaster.mdt
+    keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
 
 KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/keymaster/,$(notdir $(KM_IMAGES)))
 $(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Keymaster firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /firmware/image/keymaste$(suffix $@) $@
+	$(hide) ln -sf /firmware/image/$(suffix $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
 
