855cf947c7856543e39fae43e70017733f90af48
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2015-12-23 18:43:42 +0100

    update some audio properties
    Change-Id: I6be3a590838cd5b9c2e0093a2b470c53afca2826

--- msm8974.mk
+++ msm8974.mk
@@ -37,11 +37,15 @@ PRODUCT_COPY_FILES += \
 
 # System properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    mm.enable.smoothstreaming=true \
     audio.offload.buffer.size.kb=32 \
-    audio.offload.gapless.enabled=false \
-    av.offload.enable=true \
+    audio.offload.video=true \
+    av.streaming.offload.enable=true \
+    audio.offload.gapless.enabled=true \
+    audio.offload.pcm.16bit.enable=false \
+    audio.offload.pcm.24bit.enable=true \
     audio.deep_buffer.media=true
-    
+
 # Reduce client buffer size for fast audio output tracks
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1
