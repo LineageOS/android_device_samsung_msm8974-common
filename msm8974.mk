48ded0e94427993d751e22144edf706a300dcad8
Christopher R. Palmer <crpalmer@gmail.com>
2016-01-21 07:18:49 -0500

    msm8974-common: Move low_latency permission up to devices
    Mondrianwifi does not like having this enabled.  When it is enabled,
    it causes audio distortion in some games when using a2dp output.
    
    Partially reverts: "Reduce fast track multiplier"
    
    Change-Id: Ic50bb016275de821be4651280c67c42f64241dab

--- msm8974.mk
+++ msm8974.mk
@@ -17,7 +17,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/msm8974-common/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
@@ -39,9 +38,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=false \
     av.offload.enable=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
