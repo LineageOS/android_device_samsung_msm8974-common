79060a6bb2c62dccd8fbd696e63b2bdf6b0647a8
Ziyan <jaraidaniel@gmail.com>
2016-09-09 18:04:54 +0200

    msm8974-common: ril: add squash of OSS libsecnativefeature
     msm8974-common: ril: add open-sourced libsecnativefeature, used by libsec-ril
    
     Change-Id: I2f487db23b31c63ba919863f393ca37cc9a81191
    
     libsecnativefeature: fix build with clang
    
     Change-Id: Ibda3234af2b1184e094913e35cb919a75f972c87
    
     msm8974-common: ril: Move device dependent modules to /vendor
    
     Change-Id: Ia2f4835e3325f3d8d53db26fb632b3b9eedcc382
    
    Change-Id: Iaaa06fa527cedce6609a95b21d787c13dda6a30a

---
+++ libsecnativefeature/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+	SecNativeFeatureCIf.cpp \
+	SecNativeFeatureCppIf.cpp
+
+LOCAL_C_INCLUDES += \
+	external/expat/lib
+
+LOCAL_SHARED_LIBRARIES := \
+	libexpat
+
+LOCAL_CFLAGS := -Wall -Werror
+
+LOCAL_MODULE := libsecnativefeature
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
