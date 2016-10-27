b36f9b35c7f781e3947aa58223cd4df265f681e7
Paul Keith <javelinanddart@gmail.com>
2016-10-27 08:13:34 -0500

    msm8974-common: Add vsync phase offset
    * Kanged values from hammerhead
    * Reference:
      http://stackoverflow.com/questions/27947848/understanding-necessity-of-android-vsync-signals
    
    Change-Id: I687a306ca1a3bc8f23174a1a8b8a799baad7a3b3
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,6 +36,8 @@ BOARD_CHARGING_CMDLINE_VALUE := "true"
 # Display
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
