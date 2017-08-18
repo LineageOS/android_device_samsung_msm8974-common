6c9bcca316ce66ffe8a054948c369b68092a6793
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    msm8974-common: init: Update for O
    Change-Id: I60475e3dbf68329a46c421e8c8465a4ea2e28203

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#               2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -10,5 +26,7 @@ ifneq ($(TARGET_LIBINIT_MSM8974_DEFINES_FILE),)
   LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_MSM8974_DEFINES_FILE)
 endif
 LOCAL_MODULE := libinit_msm8974
+
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
