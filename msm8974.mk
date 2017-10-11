b4a613d04bbfc5f3c6d6eef3dc92cfad0f05f40f
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-11 19:10:52 +0300

    msm8974-common:  Add audio and display HAL impl
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba26

--- msm8974.mk
+++ msm8974.mk
@@ -59,6 +59,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio_policy.msm8974 \
     audio.a2dp.default \
     audio.primary.msm8974 \
@@ -72,9 +78,17 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
+    libgenlock \
     memtrack.msm8974
 
 # Keylayouts
