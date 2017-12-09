d4775e2067f13c11fcd7884f85c4753e1985cf07
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2017-12-09 14:00:37 +0200

    [DNM] msm8974-common: O bringup
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba16

--- msm8974.mk
+++ msm8974.mk
@@ -75,9 +75,11 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.configstore@1.0-service \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
