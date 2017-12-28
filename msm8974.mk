dd2586956df65fde165998d18c3dd60dce1b305c
Kevin F. Haggerty <haggertk@lineageos.org>
2017-12-28 15:11:44 -0700

    msm8974-common: Drop audiod
    Change-Id: I455752ca3bc0b444ef15274be03df791120bb00a

--- msm8974.mk
+++ msm8974.mk
@@ -60,7 +60,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
     audio_policy.msm8974 \
     audio.a2dp.default \
     audio.primary.msm8974 \
