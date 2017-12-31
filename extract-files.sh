7477a67fb54108f46e05e7bc027268e215e0ce60
Kevin F. Haggerty <haggertk@lineageos.org>
2017-12-31 08:43:59 -0700

    msm8974-common: Setup extractors for shared blobs
    Change-Id: I21194e86f3f755510130118a0a1b7a47a6aa1a5a

---
+++ extract-files.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+# Override anything that may come from the calling environment
+CM_ROOT="$MY_DIR"/../../..
+BOARD=msm8974
+DEVICE_COMMON=${BOARD}-common
+VENDOR=samsung
+
+HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+    SRC=adb
+else
+    if [ $# -eq 1 ]; then
+        SRC=$1
+    else
+        echo "$0: bad number of arguments"
+        echo ""
+        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+        echo ""
+        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+        echo "the device using adb pull."
+        exit 1
+    fi
+fi
+
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+
+extract "$MY_DIR"/common-proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh
---
+++ setup-makefiles.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+# Override anything that may come from the calling environment
+CM_ROOT="$MY_DIR"/../../..
+BOARD=msm8974
+DEVICE_COMMON=${BOARD}-common
+INITIAL_COPYRIGHT_YEAR=2017
+VENDOR=samsung
+
+HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+
+# Copyright headers
+write_headers $BOARD TARGET_BOARD_BOARD
+
+write_makefiles "$MY_DIR"/common-proprietary-files.txt
+
+write_footers
