efce540487f6ff71443662fcacb2c39fcb9e39c2
Paul Keith <javelinanddart@gmail.com>
2018-01-19 04:52:42 +0100

    msm8974: Unconditionally enable full dex preopt on Linux
    * So, uh, it turns out we need to disable booting with all
      but 1 CPU core to fix CDMA RIL on klte (and hlte?), so
      let's enable full dex preopt so that we don't dex opt
    
    Change-Id: I6fe57f9dcd1da8245eebd51d61055d6de7ffe6cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,12 +37,7 @@ WITH_LINEAGE_CHARGER := false
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
+  WITH_DEXPREOPT := true
 endif
 
 # Display
