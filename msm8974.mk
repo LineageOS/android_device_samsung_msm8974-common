834c175d72e0a52156ae1afcc8783ed76ad02604
Paul Keith <javelinanddart@gmail.com>
2018-02-08 19:50:59 +0100

    msm8974: Update powerhal for binderization
    * Old faithful has now been binderized
    
    Change-Id: I58cd1bdccee8d046af8995f54f1293cb8f9f3afe

--- msm8974.mk
+++ msm8974.mk
@@ -142,8 +142,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8974
+    android.hardware.power@1.0-service-cm
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
