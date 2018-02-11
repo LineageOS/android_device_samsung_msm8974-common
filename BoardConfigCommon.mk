6279ca895cc4f05e06d946901186ca6c35447f2e
razorloves <razorloves@gmail.com>
2018-02-11 03:17:25 -0600

    msm8974-common: update dexpreopt option
    The recent change we made in the build repo makes all
    builds dexpreopt only boot img and system server.
    This changes angler builds back to full dexpreopt.
    
    Change-Id: I0cf03634ee7bbc93b18b49d3f1efe82a6302b447

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,7 @@ WITH_LINEAGE_CHARGER := false
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
     WITH_DEXPREOPT := true
   endif
 endif
