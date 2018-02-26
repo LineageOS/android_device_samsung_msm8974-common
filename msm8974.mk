ff9c8cd40a1e24af96cd1fad389a4a32882b411f
Kevin F. Haggerty <haggertk@lineageos.org>
2018-02-26 06:28:32 -0700

    Revert "Revert "msm8974-common: Use QTI HIDL power HAL""
    This reverts commit 24d70a1aec4a579168c5ad8b17889f8f9b28fe8a.
    
    * The new HIDL power HAL seems to be appropriately riced^Wfixed for
      8974 now.
    
    Change-Id: I629ab2314dec4698a0d4d45d77cdc48bf92a4d74

--- msm8974.mk
+++ msm8974.mk
@@ -149,8 +149,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8974
+    android.hardware.power@1.0-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
