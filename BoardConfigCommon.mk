306c3534006bb54aa904fcfbf436ce647be788c6
Paul Keith <javelinanddart@gmail.com>
2018-03-03 05:39:35 +0100

    msm8974-common: Allow overriding full dex preopt
    * This is wonderful for users, but I want to override this
    
    Change-Id: I12b0cd7620b4615f8f3ba945fbae8a332d4ea203

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,7 @@ BOARD_HARDWARE_CLASS += device/samsung/msm8974-common/lineagehw
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= false
     WITH_DEXPREOPT := true
   endif
 endif
