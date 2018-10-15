828c5416ba161c2f8abb1e22343c383bc7bcd18f
dianlujitao <dianlujitao@lineageos.org>
2018-10-15 16:06:26 +0800

    msm8974-common: Remove power-off alarm stuff from init extension
    * Samsung msm8974 devices never supported this to begin with, even
      with the stock OS
    
    Change-Id: Ie4d30358314337c229a66a16195f675198ef7d52

--- init/init_msm8974.cpp
+++ init/init_msm8974.cpp
@@ -70,59 +70,7 @@ void property_override_dual(char const system_prop[],
     property_override(vendor_prop, value);
 }
 
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        LOG(ERROR) << "failed to open " << fname << std::endl;
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
-static void init_alarm_boot_properties()
-{
-    char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[64];
-
-    if (read_file2(alarm_file, buf, sizeof(buf))) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger insertd
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-        if (buf[0] == '3')
-            property_set("ro.alarm_boot", "true");
-        else
-            property_set("ro.alarm_boot", "false");
-    }
-}
-
 void vendor_load_properties()
 {
     init_target_properties();
-    init_alarm_boot_properties();
 }
