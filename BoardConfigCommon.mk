67de8e748464a0a4141c8a5c97b202ba90dda7b4
Bruno Martins <bgcngm@gmail.com>
2018-12-22 19:07:24 +0000

    msm8974-common: Enable B-services aging propagation
    Change-Id: I4ac0525933bb421f7d025c5e4c6a1de0f083c46e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,6 +94,9 @@ TARGET_HAS_LEGACY_POWER_STATS := true
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_USES_INTERACTION_BOOST := true
 
+# Properties
+TARGET_SYSTEM_PROP += device/samsung/msm8974-common/system.prop
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
