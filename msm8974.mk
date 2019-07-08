97314fbc133ecffa243e8d623c519d3ad7421f21
Amit Pundir <amit.pundir@linaro.org>
2019-07-08 22:17:09 +0530

    msm8974-common: Add gatekeeper software HIDL service.
    Use the default software implementation of gatekeeper.
    
    Change-Id: Id696752ad78047155cad6a5dafe7ca1b4fe86345
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- msm8974.mk
+++ msm8974.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service.software
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl.legacy \
