50cbab0988ba9ec3976f040c40a0301277c84265
Paul Keith <javelinanddart@gmail.com>
2019-11-27 15:18:27 -0800

    msm8974-common: Remove unnecessary keylayout files
    * Nothing uses these keylayout files
    
    Change-Id: I477445fd226f5fc77125fa5706e42cd8e0fe8b02

--- msm8974.mk
+++ msm8974.mk
@@ -130,12 +130,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service.samsung
 
-# Keylayouts
-PRODUCT_COPY_FILES += \
-    device/samsung/msm8974-common/keylayout/cyttsp-i2c.kl:system/usr/keylayout/cyttsp-i2c.kl \
-    device/samsung/msm8974-common/keylayout/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl \
-    device/samsung/msm8974-common/keylayout/Vendor_04e8_Product_7021.kl:system/usr/keylayout/Vendor_04e8_Product_7021.kl
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
