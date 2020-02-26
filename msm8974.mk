53fd5b082884628b7f5bebbafb1ae1e08fd1e52f
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    msm8974-common: Build and enable fastbootd
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- msm8974.mk
+++ msm8974.mk
@@ -96,6 +96,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
