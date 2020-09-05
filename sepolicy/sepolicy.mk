16a190dd28fe4c526c6290b6a2f5a062de0fdec8
Kevin F. Haggerty <haggertk@lineageos.org>
2020-09-05 05:28:09 -0600

    msm8974-common: sepolicy: Allow system_app to access storaged via IPC
    avc: denied { call } for comm=4173796E635461736B202333
    scontext=u:r:system_app:s0 tcontext=u:r:storaged:s0 tclass=binder
    permissive=t0
    
    Change-Id: I933dcebf2f5960d639ce47be379f62636e4ddd69

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -19,3 +19,5 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
     device/samsung/msm8974-common/sepolicy/common
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/samsung/msm8974-common/sepolicy/private
