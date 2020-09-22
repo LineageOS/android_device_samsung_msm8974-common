684a57b5dcb89cf246cf396a63511ab789faf6fe
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    msm8974-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- msm8974.mk
+++ msm8974.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
