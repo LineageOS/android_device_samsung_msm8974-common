8a56039c7d40971e894fea7b5204775a56368873
Arne Coucheron <arco68@gmail.com>
2020-11-22 01:18:39 +0100

    msm8974-common: Set TARGET_DISABLE_POSTRENDER_CLEANUP := true
    To fix the bad UI lag we are experiencing.
    
    Change-Id: I3edb25f93c58b87431a1802726f31123200f75d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,7 @@ endif
 # Display
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U
+TARGET_DISABLE_POSTRENDER_CLEANUP := true
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
