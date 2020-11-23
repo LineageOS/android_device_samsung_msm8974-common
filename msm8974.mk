87e7dd6201cc35268ea3142cd67d447bb4fe8a9d
Nolen Johnson <johnsonnolen@gmail.com>
2020-11-23 14:46:29 -0500

    msm8974-common: Cleanup dead targets
    Offending entries:
    audio_policy.msm8974
    build/make/core/main.mk:1136: error: Build failed.
    
    Change-Id: I0e4d93f4c3be077d49befa1f547d11d623ca74d5

--- msm8974.mk
+++ msm8974.mk
@@ -49,7 +49,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    audio_policy.msm8974 \
     audio.a2dp.default \
     audio.primary.msm8974 \
     audio.r_submix.default \
