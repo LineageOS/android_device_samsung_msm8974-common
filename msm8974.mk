5c6a1eabd68653e0279e3d8bbc99abcc61aac3c2
Kevin F. Haggerty <haggertk@lineageos.org>
2020-11-28 07:17:09 -0700

    msm8974-common: Use libprotobuf-cpp-lite-v29.so for libwvdrmengine.so
    * libwvdrmengine.so needs _ZN6google8protobuf8internal13empty_string_E
      which is dropped in the API 30 library, but can be found in the
      API 29 prebuilt
    
    Change-Id: I39e3251d53b14f3325282b12e6a6bf741ebcf63a

--- msm8974.mk
+++ msm8974.mk
@@ -197,6 +197,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service.legacy \
