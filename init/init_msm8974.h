0b340d01ad3989494fae990a5756460594a14fab
Kevin F. Haggerty <haggertk@lineageos.org>
2021-01-04 11:52:49 -0700

    msm8974-common: Update default behavior of property_override()
    * Prior to R, we used property_set() to add properties. In the R update
      we migrated this to using property_override(), which has the ability
      to add a property, but we didn't set the parameter of the method. We
      could go and do that for all of those instances of use, but there
      it's really any time where we *don't* want to add these properties
      in our libinit extension.
    
    Change-Id: Iabf66d5bbaf13b2124b0062bb0a184d99bb59462

--- init/init_msm8974.h
+++ init/init_msm8974.h
@@ -39,7 +39,7 @@ void cdma_properties(char const operator_alpha[],
 void gsm_properties(char const default_network[],
         char const rild_lib_variant[]);
 void init_target_properties();
-void property_override(char const prop[], char const value[], bool add=false);
+void property_override(char const prop[], char const value[], bool add=true);
 void set_ro_product_prop(char const prop[], char const value[]);
 
 #endif /* __INIT_MSM8974__H__ */
