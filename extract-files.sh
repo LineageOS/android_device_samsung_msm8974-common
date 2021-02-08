345ae3b0f6fc4331a812fd1819235d9bdec3aced
Michael Bestas <mkbestas@lineageos.org>
2021-02-08 04:55:55 -0700

    msm8974-common: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,66 +1,43 @@
 #!/bin/bash
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-# Override anything that may come from the calling environment
-CM_ROOT="$MY_DIR"/../../..
-BOARD=msm8974
-DEVICE_COMMON=${BOARD}-common
-VENDOR=samsung
-
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+if [ -z "${BOARD_COMMON}" ]; then
+    echo ""
+    echo "error: This is a script in a common tree. Please execute" $(basename $0) "from a device tree."
+    echo ""
     exit 1
 fi
-. "$HELPER"
+
+# Override anything that may come from the calling environment
+DEVICE_COMMON="${BOARD_COMMON}"
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
         vendor/lib/mediadrm/libwvdrmengine.so)
-            patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so "${2}"
+            "${PATCHELF}" --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so "${2}"
             ;;
     esac
 }
 
-if [ $# -eq 0 ]; then
-    SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
-fi
+setup_vendor "${BOARD_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
-
-extract "$MY_DIR"/common-proprietary-files.txt "$SRC"
-
-"$MY_DIR"/setup-makefiles.sh
+extract "${MY_DIR}/common-proprietary-files.txt" "${SRC}"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
+if [ -z "${BOARD_COMMON}" ]; then
+    echo ""
+    echo "error: This is a script in a common tree. Please execute" $(basename $0) "from a device tree."
+    echo ""
+    exit 1
+fi
+
+# Override anything that may come from the calling environment
+BOARD=msm8974
+DEVICE_COMMON="${BOARD_COMMON}"
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-# Override anything that may come from the calling environment
-CM_ROOT="$MY_DIR"/../../..
-BOARD=msm8974
-DEVICE_COMMON=${BOARD}-common
-INITIAL_COPYRIGHT_YEAR=2017
-VENDOR=samsung
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+# Initialize the helper for common
+setup_vendor "${BOARD_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-# Copyright headers
-write_headers $BOARD TARGET_BOARD_PLATFORM
+# Warning headers and guards
+write_headers "${BOARD}" "TARGET_BOARD_PLATFORM"
 
-write_makefiles "$MY_DIR"/common-proprietary-files.txt
+# The standard common blobs
+write_makefiles "${MY_DIR}/common-proprietary-files.txt" true
 
+# Finish
 write_footers
